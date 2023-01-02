d41d77040b37ebd7d87573ee12d18af26126a336
Monkeychou1102 <otto8888@hotmail.com>
2023-01-02 11:12:01 +0800

    Add backward moving action

--- Q_Spider_PCA9685/Spider_Movement_Control.cpp
+++ Q_Spider_PCA9685/Spider_Movement_Control.cpp
@@ -85,10 +85,17 @@ void Spider_Forward(char state, unsigned int delay_time)
     {
         Spider_Standby();
         delay(delay_time);
+        Front_Right(F_R_Leg_45, F_R_Foot_135);
+        delay(delay_time);
+        Front_Right(F_R_Leg_0, F_R_Foot_135);
+        delay(delay_time);
+        Front_Right(F_R_Leg_0, F_R_Foot_90);
+        delay(delay_time);
     }
     else if (state == 1)
     {
         Front_Right(F_R_Leg_0, F_R_Foot_90);
+        delay(delay_time);
         Back_Right(B_R_Leg_n45, B_R_Foot_135);
         delay(delay_time);
         Back_Right(B_R_Leg_0, B_R_Foot_135);
@@ -103,6 +110,7 @@ void Spider_Forward(char state, unsigned int delay_time)
         Front_Right(F_R_Leg_Max, F_R_Foot_135);
         delay(delay_time);
         Front_Right(F_R_Leg_Max, F_R_Foot_90);
+        delay(delay_time);
     }
     else if (state == 3)
     {
@@ -151,57 +159,65 @@ void Spider_Backward(char state, unsigned int delay_time)
     {
         Spider_Standby();
         delay(delay_time);
+        Back_Right(B_R_Leg_Min, B_R_Foot_135);
+        delay(delay_time);
+        Back_Right(B_R_Leg_0, B_R_Foot_135);
+        delay(delay_time);
+        Back_Right(B_R_Leg_0, B_R_Foot_90);
+        delay(delay_time);
     }
     else if (state == 1)
     {
-        Back_Left(F_R_Leg_0, F_R_Foot_90);
-        Front_Left(B_R_Leg_n45, B_R_Foot_135);
+        Back_Right(B_R_Leg_0, B_R_Foot_90);
         delay(delay_time);
-        Front_Left(B_R_Leg_0, B_R_Foot_135);
+        Front_Right(F_R_Leg_45, F_R_Foot_135);
         delay(delay_time);
-        Front_Left(B_R_Leg_0, B_R_Foot_90);
+        Front_Right(F_R_Leg_0, F_R_Foot_135);
+        delay(delay_time);
+        Front_Right(F_R_Leg_0, F_R_Foot_90);
         delay(delay_time);
     }
     else if (state == 2)
     {
-        Back_Left(F_R_Leg_0, F_R_Foot_135);
+        Back_Right(B_R_Leg_0, B_R_Foot_135);
         delay(delay_time);
-        Back_Left(F_R_Leg_Max, F_R_Foot_135);
+        Back_Right(B_R_Leg_Min, B_R_Foot_135);
         delay(delay_time);
-        Back_Left(F_R_Leg_Max, F_R_Foot_90);
+        Back_Right(B_R_Leg_Min, B_R_Foot_90);
+        delay(delay_time);
     }
     else if (state == 3)
     {
-        Back_Right(F_L_Leg_0, F_L_Foot_90);
-        Back_Left(F_R_Leg_45, F_R_Foot_90);
-        Front_Right((B_L_Leg_Min - 200), B_L_Foot_90);
-        Front_Left((B_R_Leg_n45 - 200), B_R_Foot_90);
+        Front_Left(F_L_Leg_Max, F_L_Foot_90);
+        Front_Right(F_R_Leg_45, F_R_Foot_90);
+        Back_Left(B_L_Leg_0, B_L_Foot_90);
+        Back_Right(B_R_Leg_n45, B_R_Foot_90);
         delay(delay_time * 2);
     }
     else if (state == 4)
     {
-        Front_Right(B_L_Leg_Min, B_L_Foot_135);
+        Front_Left(F_L_Leg_Max, F_L_Foot_135);
         delay(delay_time);
-        Front_Right(B_L_Leg_0, B_L_Foot_135);
+        Front_Left(F_L_Leg_0, F_L_Foot_135);
         delay(delay_time);
-        Front_Right(B_L_Leg_0, B_L_Foot_90);
+        Front_Left(F_L_Leg_0, F_L_Foot_90);
         delay(delay_time);
     }
     else if (state == 5)
     {
-        Back_Right(F_L_Leg_0, F_L_Foot_135);
+        Back_Left(B_L_Leg_0, B_L_Foot_135);
         delay(delay_time);
-        Back_Right(F_L_Leg_Max, F_L_Foot_135);
+        Back_Left(B_L_Leg_Min, B_L_Foot_135);
         delay(delay_time);
-        Back_Right(F_L_Leg_Max, F_L_Foot_90);
+        Back_Left(B_L_Leg_Min, B_L_Foot_90);
         delay(delay_time);
     }
     else if (state == 6)
     {
-        Back_Right(F_L_Leg_45, F_L_Foot_90);
-        Back_Left(F_R_Leg_0, F_R_Foot_90);
-        Front_Right(B_L_Leg_n45, B_L_Foot_90);
-        Front_Left(B_R_Leg_Min, B_R_Foot_90);
+        Front_Left(F_L_Leg_45, F_L_Foot_90);
+        Front_Right(F_R_Leg_Max, F_R_Foot_90);
+        Back_Left(B_L_Leg_n45, B_L_Foot_90);
+        Back_Right(B_R_Leg_0, B_R_Foot_90);
         delay(delay_time * 2);
     }
     else
